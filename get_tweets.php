82a1893fbc1d4bfb6f7639446722a20e131be9a2
Regnier <brett.regnier@uleth.ca>
2019-03-25 12:52:08 -0600

    initial commit

---
+++ get_tweets.php
@@ -0,0 +1,25 @@
+<?php
+
+	require_once('TwitterAPIExchange.php');
+	 
+	// Set access tokens: https://dev.twitter.com/apps/
+	$settings = array(
+	    'oauth_access_token' => "1110227370001104896-M70Uv6rNmHQAT5LoP7IGbzWnuGBG8O",
+	    'oauth_access_token_secret' => "kk0XctyCAS6eR22QIxiEXSn6DTRQHW4u4sbJvKIphjquH",
+	    'consumer_key' => "ZOI07gBnR5kFsOhhlz3zT61DW",
+	    'consumer_secret' => "hCX4uSTT3fGTuhhiPRr7cqxIYWbeEWZ7OiH9lIOkUGccvYNs51"
+	);
+
+	// Choose URL and Request Method
+	$url = "https://api.twitter.com/1.1/search/tweets.json";
+	$getfield = '?q=#technology'; // queries start with ? and are strung together with &
+	$requestMethod = "GET";
+	
+	//Perform the request!
+	$twitter = new TwitterAPIExchange($settings);
+	echo $twitter->setGetfield($getfield)
+	             ->buildOauth($url, $requestMethod)
+	             ->performRequest();
+
+?>
+
